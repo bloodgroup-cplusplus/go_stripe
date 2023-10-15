619c238357587100696d7e53cd58efdb16f2df87
bloodgroup-cplusplus <81256027+bloodgroup-cplusplus@users.noreply.github.com>
2023-10-16 05:19:49 +0530

    handling templates with and without partials and with and without data building all the conditions

--- cmd/web/render.go
+++ cmd/web/render.go
@@ -2,6 +2,9 @@ package main
 
 import (
 	"embed"
+	"fmt"
+	"net/http"
+	"strings"
 	"text/template"
 )
 
@@ -28,3 +31,69 @@ var functions = template.FuncMap{}
 // go:embed embed the directory templates
 
 var templateFS embed.FS
+
+func (app *application) addDefaultData(td *templateData, r *http.Request) *templateData {
+	return td
+}
+
+// write a function that renders a template
+func (app *application) renderTemplate(w http.ResponseWriter, r *http.Request, page string, td *templateData, partials ...string) error {
+	//create a variable
+	var t *template.Template
+	var err error
+	templateToRender := fmt.Sprintf("templates/%s.page.tmpl", page)
+	//check to see if we have the template cache
+	_, templateInMap := app.templateCache[templateToRender]
+	// when never want to use template cache
+	if app.config.env == "production" && templateInMap {
+		t = app.templateCache[templateToRender]
+	} else {
+		t, err = app.parseTemplate(partials, page, templateToRender)
+		if err != nil {
+			app.errorLog.Println(err)
+			return err
+		}
+	}
+	if td == nil {
+		td = &templateData{}
+	}
+	td = app.addDefaultData(td, r)
+
+	err = t.Execute(w, td)
+
+	if err != nil {
+		app.errorLog.Println(err)
+		return err
+	}
+
+	return nil
+}
+
+func (app *application) parseTemplate(partials []string, page, templateToRender string) (*template.Template, error) {
+
+	// declare variables
+	var t *template.Template
+	var err error
+
+	// build partials
+	if len(partials) > 0 {
+		for i, x := range partials {
+			partials[i] = fmt.Sprintf("templates/%s.partial.tmpl", x)
+		}
+	}
+	if len(partials) > 0 {
+		t, err = template.New(fmt.Sprintf("%s.page.tmpl", page)).Funcs(functions).ParseFS(templateFS, "templates/base.layout.tmpl", strings.Join(partials, ","), templateToRender)
+
+	} else {
+		t, err = template.New(fmt.Sprintf("%s.page.tmpl", page)).Funcs(functions).ParseFS(templateFS, "templates/base.layout.tmpl", strings.Join(partials, ","), templateToRender)
+
+	}
+	if err != nil {
+		app.errorLog.Println(err)
+		return nil, err
+	}
+	app.templateCache[templateToRender] = t
+
+	return t, nil
+
+}
