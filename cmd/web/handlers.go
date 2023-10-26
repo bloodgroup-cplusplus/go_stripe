1d9f34767687681f256aad431ce990aea6e2471a
bloodgroup-cplusplus <81256027+bloodgroup-cplusplus@users.noreply.github.com>
2023-10-26 10:57:27 +0530

    added terminal templates with error

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -7,6 +7,8 @@ import (
 // set up a really simple handlers
 
 func (app *application) VirtualTerminal(w http.ResponseWriter, r *http.Request) {
-	app.infoLog.Println("Hit the handler")
+	if err := app.renderTemplate(w, r, "terminal", nil); err != nil {
+		app.errorLog.Println(err)
+	}
 
 }
